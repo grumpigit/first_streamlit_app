a2eb599f7ec98afb4ccb09103ed3f1befb47f8eb
grumpigit <42210826+grumpigit@users.noreply.github.com>
2022-08-06 13:56:35 +0100

    test3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
+fruits_selected = my_fruit_list.index
 
 streamlit.title('My Parents New Healty Diner')
 
