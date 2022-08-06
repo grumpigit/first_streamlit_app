163fc0ff72ca2f416e7480ea13a20bf09f4806e9
grumpigit <42210826+grumpigit@users.noreply.github.com>
2022-08-06 13:52:12 +0100

    test1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_to_show = my_fruit_list.loc['Avocado', 'Strawberries']
+fruits_to_show = my_fruit_list.loc[]
 
 streamlit.title('My Parents New Healty Diner')
 
