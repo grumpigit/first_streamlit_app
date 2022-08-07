eab31d8121868623762682c7be6ef7c5efb917c4
grumpigit <42210826+grumpigit@users.noreply.github.com>
2022-08-07 14:36:12 +0100

    typo too many double qs

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + " "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # prettify the json payload into a normalized table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
