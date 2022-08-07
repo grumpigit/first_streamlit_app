d8ed5996766dc30dc082b87de70a02cbb6533523
grumpigit <42210826+grumpigit@users.noreply.github.com>
2022-08-07 14:35:01 +0100

    added df and normalised data object to carry the json payload removed raw

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + " "kiwi")
+
+# prettify the json payload into a normalized table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# pops it into a dataframe
+streamlit.dataframe(fruityvice_normalized)
