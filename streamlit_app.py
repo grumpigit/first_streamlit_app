bf22f23895144a74810242683fd6dcc5f3bfa72e
grumpigit <42210826+grumpigit@users.noreply.github.com>
2022-08-07 16:01:49 +0100

    stop running the snowflake code

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # pops it into a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
